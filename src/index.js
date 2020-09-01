10f553e02072b09e7b4052d132c625516a988b89
Oroko <aoroko09@gmail.com>
2020-09-01 16:53:01 -0400

    Created a Credit card and used props to pass in data and used css with pisitioning property to style the elements

--- src/index.js
+++ src/index.js
@@ -3,38 +3,35 @@
 // component
 
 import React from "react";
-import PropTypes from 'prop-types'
+//import PropTypes from 'prop-types'
 import ReactDOM from "react-dom";
 import './index.css'
 
-const AddressLabel = ({info}) => {
+const CreditCard =({info}) => {
+  const {name,expirationDate,creditCardNumber,bankName} = info
   return (
-    <div className="address-label">
-    <p>{info.fullName}</p>
-    <p>{info.streetAddress}</p>
-    <p>{info.zipCode}</p>
-    
+    <div className="credit-card">
+      <div className="bank-name"> {name}</div>
+      <div className="card-number">{creditCardNumber}</div>
+      <div className="first-four-digits">{creditCardNumber.substring(0,4)}</div>
+      <div className="expiration">
+        <span className="valid-thru">Valid Thru</span> {expirationDate}
+      </div>
+      <div className="bank">{bankName}</div>
     </div>
   )
 }
 
-const person = {
-  fullName: "Abel Gechure",
-  streetAddress: "1117 Birch Lane" ,
-  zipCode: "Phillipsburg, NJ 08865 "
+const cardInfo = {
+  name: "Abel Gechure",
+  expirationDate: "08/19",
+  creditCardNumber: "1234 5678 8765 4321",
+  bankName: "Big Bank, Inc."
 }
 
-AddressLabel.propTypes = {
-  info:PropTypes.shape({
-    fullName: PropTypes.string.isRequired,
-    streetAddress: PropTypes.string.isRequired,
-    zipCode: PropTypes.string.isRequired
-  })
-
-}
 
 
 ReactDOM.render(
-  <AddressLabel info={person} />,
+  <CreditCard info={cardInfo}/>,
   document.querySelector("#root")
 )
